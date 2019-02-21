4bc99a6554c18292b05eaf2e52dd106e86032934
Kyle Carter <kylecarter@zipcoders-MBP-9.T-mobile.com>
2019-02-20 21:35:42 -0500

    bins class and unit tests complete

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,24 @@
 public class Simulation {
 
 
+    private int numberOfDice;
+    private int numberOfThrows;
+    private Dice dice;
+    private Bins bin;
 
+    public Simulation(int numberOfDice, int numberOfThrows) {
+        this.numberOfDice = numberOfDice;
+        this.numberOfThrows = numberOfThrows;
+        this.bin = new Bins(numberOfDice, numberOfDice*6);
+        this.dice = new Dice(numberOfDice);
+    }
+
+    public int getNumberOfDice() {
+        return numberOfDice;
+
+    }
+
+    public int getNumberOfThrows() {
+        return numberOfThrows;
+    }
 }
